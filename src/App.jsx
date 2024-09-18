7a997c248622ef0625e271585c1f6e75cdd9db5d
DahalPratik <pradahal545@gmail.com>
2024-09-18 11:06:58 +0545

    Added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: red;
-`;
-
-const Button = styled.button`
-  font-size: 1.2rem;
-  padding: 0.5rem 1rem;
-  font-weight: 600;
-  border: none;
-  border-radius: 5px;
-  background-color: blue;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: aqua;
   padding: 20px;
 `;
+
 function App() {
   return (
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>HotelHub</H1>
-        <Button onClick={() => alert("CheckIn")}>Check In</Button>
-        <Button onClick={() => alert("CheckOut")}>Check Out</Button>
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">HotelHub</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("CheckIn")}>Check In</Button>
+              <Button
+                onClick={() => alert("CheckOut")}
+                size="small"
+                variation="secondary"
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h2">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
---
+++ src/ui/Row.jsx
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  ${(props) =>
+    props.type === "horizontal" &&
+    css`
+      justify-content: space-between;
+      align-items: center;
+    `}
+  ${(props) =>
+    props.type === "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
+`;
+Row.defaultProps = {
+  type: "vertical",
+};
+//we can just use row tag only in app and it still will be vertical
+
+export default Row;
